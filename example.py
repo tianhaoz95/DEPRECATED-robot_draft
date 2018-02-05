809c5a12831f80bc0516082b3979d88b2e04c561
tianhaoz95 <tianhaoz@umich.edu>
2018-02-05 14:31:10 -0800

    update

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 from WallE.Robot.Robot import Robot
 from WallE.Task.Routine import SimpleRoutine
 
-robot = Robot('local', {'module_name': 'hal', 'class_name': 'HalAPI'})
+robot = Robot('local', {'module_name': 'example_robot.hal', 'class_name': 'HalAPI'})
 routine = SimpleRoutine()
 routine.add(robot, 'local', {'module_name':'example_task.task', 'class_name':'MoveTask'})
 routine.add(robot, 'remote', {'module_name': 'example_remote_task-master.task', 'class_name': 'MoveTask', 'url': 'https://github.com/tianhaoz95/example_remote_task/archive/master.zip'})
